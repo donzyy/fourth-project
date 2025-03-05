3299d1429431f1445094ea3fca18688ed0242a85
michaelperryjnr <michaelperryt97@gmail.com>
2025-03-05 23:24:13 +0000

    Added products route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import DefaultLayout from "./Layout/DefaultLayout"
-import Footer from "./Components/Footer"
-import Contactus from "./Pages/ContactUs/Contactus"
-import Map from "./Components/Map"
-import GetAQuote from "./Pages/GetAQuote/GetAQuote"
-import Landing from "./Pages/LandingPage/Landing"
-import AboutUs from "./Pages/AboutUs/AboutUs"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./Layout/DefaultLayout";
+import Footer from "./Components/Footer";
+import Contactus from "./Pages/ContactUs/Contactus";
+import Map from "./Components/Map";
+import GetAQuote from "./Pages/GetAQuote/GetAQuote";
+import Landing from "./Pages/LandingPage/Landing";
+import AboutUs from "./Pages/AboutUs/AboutUs";
+import Products from "./Pages/Products/Products";
 
 function App() {
   return (
@@ -14,10 +15,11 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/contactus" element={<Contactus />} />
-        <Route path="/quote" element={<GetAQuote/>} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/quote" element={<GetAQuote />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
